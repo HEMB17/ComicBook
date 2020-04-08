76050d4bd46d3f2899f9ceb966244b2f1e29efb6
emmanuel03 <mbhe.03@gmail.com>
2020-04-08 12:01:11 -0500

    lazy loading

--- app/src/main/java/com/example/comicbook/activities/MainActivity.java
+++ app/src/main/java/com/example/comicbook/activities/MainActivity.java
@@ -41,69 +41,113 @@ public class MainActivity extends AppCompatActivity {
     List<BookItem> mList = new ArrayList<>();
     ProgressDialog progressDoalog;
 
+    private boolean loading = true;
+    int pastVisiblesItems, visibleItemCount, totalItemCount;
+    int offset = 0;
+    List<BookItem> listBook = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerBook = findViewById(R.id.recyclerBooks);
 
 
         if(isNetworkAvailable(this)) {
 
-            progressDoalog = new ProgressDialog(MainActivity.this);
-            progressDoalog.setMessage("Loading....");
-            progressDoalog.show();
-
-            GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
-            Call<ResultModel<List<BookItem>>> call = service.getAllBooks();
-            call.enqueue(new Callback<ResultModel<List<BookItem>>>() {
-                @Override
-                public void onResponse(Call<ResultModel<List<BookItem>>> call, Response<ResultModel<List<BookItem>>> response) {
-                    progressDoalog.dismiss();
-                    generateDataList(response.body().getResults());
-                }
-
-                @Override
-                public void onFailure(Call<ResultModel<List<BookItem>>> call, Throwable error) {
-                    progressDoalog.dismiss();
-                    if (error instanceof SocketTimeoutException)
-                    {
-                        Toast.makeText(MainActivity.this, "Connection Timeout", Toast.LENGTH_SHORT).show();
-                    }
-                    else if (error instanceof IOException)
-                    {
-                        Toast.makeText(MainActivity.this, "Timeout", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        //Call was cancelled by user
-                        if(call.isCanceled())
-                        {
-                            Toast.makeText(MainActivity.this, "Call was cancelled forcefully", Toast.LENGTH_SHORT).show();
-                        }
-                        else
-                        {
-                            //Generic error handling
-                            Toast.makeText(MainActivity.this, "Network Error :: " + error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-            });
+            getBook(offset, 10);
+            generateDataList();
 
         }else{
             Toast.makeText(MainActivity.this, "Not Internet Connection", Toast.LENGTH_SHORT).show();
         }
 
+        recyclerBook.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0) { //check for scroll down
+                    visibleItemCount = gridLayoutManager.getChildCount();
+                    totalItemCount = gridLayoutManager.getItemCount();
+                    pastVisiblesItems = gridLayoutManager.findFirstVisibleItemPosition();
+
+                    if( loading){
+                        if ((visibleItemCount + pastVisiblesItems) >= totalItemCount) {
+                            loading = false;
+                            offset += 10;
+
+                            getBook(offset  , 10);
+                            //Toast.makeText(MainActivity.this, "Last Item Wow !", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+                }
+            }
+        });
+
 
     }
 
-    private void generateDataList(List<BookItem> mList) {
+    private void generateDataList() {
 
         gridLayoutManager = new GridLayoutManager(this, SPAN_COUNT_ONE);
-        recyclerBook = findViewById(R.id.recyclerBooks);
-        mAdapter = new RecyclerBookAdapter(mList, this, gridLayoutManager);
+        mAdapter = new RecyclerBookAdapter(listBook, this, gridLayoutManager);
         recyclerBook.setAdapter(mAdapter);
         recyclerBook.setLayoutManager(gridLayoutManager);
 
+    }
+
+    public void getBook(int offset, int limit){
+
+        progressDoalog = new ProgressDialog(MainActivity.this);
+        progressDoalog.setMessage("Loading....");
+        progressDoalog.show();
+
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<ResultModel<List<BookItem>>> call = service.getAllBooks("1ed7967e84b13642e18f74eae0c8e399d0ddbe9b",offset,limit);
+        call.enqueue(new Callback<ResultModel<List<BookItem>>>() {
+            @Override
+            public void onResponse(Call<ResultModel<List<BookItem>>> call, Response<ResultModel<List<BookItem>>> response) {
+
+                progressDoalog.dismiss();
+
+                for(BookItem unListBook : response.body().getResults()){
+                    listBook.add(unListBook);
+                }
+
+
+                mAdapter.notifyDataSetChanged();
+                loading = true;
+
+            }
+
+            @Override
+            public void onFailure(Call<ResultModel<List<BookItem>>> call, Throwable error) {
+                progressDoalog.dismiss();
+                if (error instanceof SocketTimeoutException)
+                {
+                    Toast.makeText(MainActivity.this, "Connection Timeout", Toast.LENGTH_SHORT).show();
+                }
+                else if (error instanceof IOException)
+                {
+                    Toast.makeText(MainActivity.this, "Timeout", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //Call was cancelled by user
+                    if(call.isCanceled())
+                    {
+                        Toast.makeText(MainActivity.this, "Call was cancelled forcefully", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        //Generic error handling
+                        Toast.makeText(MainActivity.this, "Network Error :: " + error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
     public static boolean isNetworkAvailable(Context context) {
--- app/src/main/java/com/example/comicbook/services/GetDataService.java
+++ app/src/main/java/com/example/comicbook/services/GetDataService.java
@@ -11,11 +11,12 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 import retrofit2.http.Url;
 
 public interface GetDataService {
-    @GET("issues/?api_key=1ed7967e84b13642e18f74eae0c8e399d0ddbe9b&format=json&field_list=api_detail_url,date_added,name,issue_number,image")
-    Call<ResultModel<List<BookItem>>> getAllBooks();
+    @GET("issues/?format=json&field_list=api_detail_url,date_added,name,issue_number,image")
+    Call<ResultModel<List<BookItem>>> getAllBooks(@Query("api_key") String api_key, @Query("offset") int offset, @Query("limit") int limit);
 
     @GET
     Call<ResultModel<DetailBookItem>> getSubItem(@Url String url);
